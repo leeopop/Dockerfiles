048bee7439188f7fa3ef71d1b0029821e974b798
Keunhong Lee <dlrmsghd@gmail.com>
2021-11-03 01:49:55 +0000

    permission fix

--- dpdk/run.sh
+++ dpdk/run.sh
@@ -2,5 +2,9 @@
 cd /init-scripts
 source ./setenv.sh
 
+if [ -d "/workspace" ] ; then
+  chmod 777 /workspace
+fi
+
 su - $USER_NAME /init-scripts/link_workspace.sh
 /init-scripts/sshd.sh
--- node/run.sh
+++ node/run.sh
@@ -5,5 +5,9 @@ source ./setenv.sh
 # Node image 는 username 고정
 export USER_NAME=node
 
+if [ -d "/workspace" ] ; then
+  chmod 777 /workspace
+fi
+
 su - $USER_NAME /init-scripts/link_workspace.sh
 /init-scripts/sshd.sh
