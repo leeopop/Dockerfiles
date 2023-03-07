2ba82ea1bb324c5b4bce4dc91a73ae072bfb99e4
Keunhong Lee <dlrmsghd@gmail.com>
2023-03-07 15:18:47 +0000

    save

--- init_env.sh
+++ init_env.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-if [[ -f .env ]]; then
-    exit 0
-done
+if [[ ! -f .env ]]; then
 USER_NAME=${USER_NAME:-${USER}}
 if [[ -z "${ROOT_PASSWORD}" ]]; then
     ROOT_PASSWORD=`mkpasswd`
-done
+fi
 if [[ -z "${SERVICE_HOST}" ]]; then
     LOCAL_IP_LIST=(`/sbin/ifconfig ${IFACE_NAME} | grep "inet " | awk '{print $2}'`)
     SERVICE_HOST=${LOCAL_IP_LIST[0]}
-done
+fi
 cat > .env <<EOF
 USER_NAME=$USER_NAME
 SERVICE_HOST=$SERVICE_HOST
-ROOT_PASSWORD=$ROOT_PASSWORD
 GIT_PASSWORD=$GIT_PASSWORD
 EOF
+fi
+if [[ ! -f .password ]]; then
+cat > .password <<EOF
+$ROOT_PASSWORD
+EOF
+fi
--- setenv.sh
+++ setenv.sh
@@ -5,5 +5,9 @@ fi
 if [ ! -f .services ]; then
     touch .services
 fi
+if [ ! -f .password ]; then
+    touch .password
+fi
 export $(grep -v '^#' .env | xargs)
 export SERVICE_LIST=`cat .services`
+export ROOT_PASSWORD=`cat .password`
