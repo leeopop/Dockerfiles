281e5d0fd4a0e53f7eb4c970e16389136b41a8ed
Keunhong Lee <dlrmsghd@gmail.com>
2023-03-07 15:28:27 +0000

    touch

--- scripts/init_user.sh
+++ scripts/init_user.sh
@@ -6,15 +6,15 @@ echo $GIT_PASSWORD > /shared_env/GIT_PASSWORD
 chmod -R +r /shared_env
 
 usermod --password "${ROOT_PASSWORD}" root
-if id "USER_NAME" &>/dev/null; then
-  echo "USER $USER_NAME already exists"
+if id ${USER_NAME} &>/dev/null; then
+  echo "USER ${USER_NAME} already exists"
 else
-  useradd -ms /bin/bash "$USER_NAME"
+  useradd -ms /bin/bash ${USER_NAME}
 fi
 
 (
 cat /init-scripts/init_user_config.sh
 (cat /install-scripts/* 2>/dev/null || true)
-) | su - $USER_NAME
+) | su - ${USER_NAME}
 
 rm -rf /shared_env
