46d10d520a587a0f2633a3919dd93285e8c43c08
Keunhong Lee <khl@furiosa.ai>
2026-01-22 13:13:11 +0900

    minimal rustup installation

--- rust/install_rust.sh
+++ rust/install_rust.sh
@@ -1,4 +1,5 @@
 # Beginning of rust user install
-curl -f --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y --profile complete
+curl -f --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y --default-toolchain none
 source ~/.profile
+rustup --version
 # End of rust user install
